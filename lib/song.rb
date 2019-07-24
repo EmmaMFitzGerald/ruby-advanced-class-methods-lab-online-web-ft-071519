3c05259e6d6c5d9d4e4b62662e47d15e8a3ee1d1
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-24 18:24:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,8 +54,6 @@ class Song
   
   def self.new_from_filename(filename)
     song_name = filename.split(" - ")[1].chomp(".mp3")
-    artist_name = filename.split(" - ")[0].chomp(".mp3")
-    song.artist_name = artist_name
     self.new_by_name(song)
     binding.pry
   end
