2523220b0536ec2d7b0dd4b55c6c4fbc1c9e224d
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-24 17:06:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,12 @@ class Song
     x
   end
   
+   def self.new_by_name(the_name_of_song)
+    song = self.new
+    song.name = the_name_of_song
+    song
+  end
+  
   def self.new_by_name(song_name)
     @name = song_name
     @name
